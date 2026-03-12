88cf179fb098f390a830f2a31f135eabe395debf
KYSWAPP <kyswatravel@gmail.com>
2026-03-12 23:41:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-const CACHE='kyswa-v2';
-const FILES=['/', '/index.html'];
-self.addEventListener('install', e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch', e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
