32a0556f047b624d57fad071570f8a825d630bef
KYSWAPP <kyswatravel@gmail.com>
2026-03-12 23:43:33 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,2 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-  );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))));
